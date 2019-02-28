b88a5be0158ec56bb2108fc5bcaca3125b617ad3
Sebastian <sebastiangutierrezcorrea@gmail.com>
2019-02-27 22:47:59 -0500

    Fix v-card-actions component
    Add icons to EventCard content

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -3,18 +3,24 @@
     <v-layout>
       <v-card-title primary-title>
         <v-flex>
-          <div class="headline">{{ event.title }}</div>
-          <div>@{{ event.time }} on {{ event.date }}</div>
-          <div>{{ event.attendees.length }} attending</div>
+          <div class="headline mb-2">{{ event.title }}</div>
+          <div class="mb-2">
+            <v-icon class="mr-2">calendar_today</v-icon>
+            <span class="body-2">@{{ event.time }} on {{ event.date }}</span>
+          </div>
+          <div class="mb-2">
+            <v-icon class="mr-2">supervised_user_circle</v-icon>
+            <span>{{ event.attendees.length }} attending</span>
+          </div>
         </v-flex>
       </v-card-title>
     </v-layout>
     <v-divider></v-divider>
-    <v-card-action>
+    <v-card-actions>
       <v-btn flat :to="{ name: 'event-show', params: { id: id } }">
         Show Event
       </v-btn>
-    </v-card-action>
+    </v-card-actions>
   </v-card>
 </template>
 
