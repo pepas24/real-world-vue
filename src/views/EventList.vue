372a810f0ae437ecff14c1d0be592133f346bfab
Sebastian <sebastiangutierrezcorrea@gmail.com>
2019-03-16 22:50:40 -0500

    Move Events fetching to Vuex store

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -13,25 +13,15 @@
 
 <script>
 import EventCard from '../components/EventCard'
-import EventService from '@/services/EventService'
+import { mapState } from 'vuex'
 
 export default {
   components: {
     EventCard
   },
-  data() {
-    return {
-      events: []
-    }
-  },
   created() {
-    EventService.getEvents()
-      .then(response => {
-        this.events = response.data
-      })
-      .catch(error => {
-        console.log('There was an error: ' + error)
-      })
-  }
+    this.$store.dispatch('fetchEvents')
+  },
+  computed: mapState(['events'])
 }
 </script>
